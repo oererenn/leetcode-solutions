fd60e1bfb57a5ffbd49f2b542e1efee5454b7fdd
Onur Ereren <onur.ereren998@gmail.com>
2023-01-25 18:09:25 +0100

    findMaxConsecutiveOnes

---
+++ src/main/java/org/algorithms/findMaxConsecutiveOnes.java
@@ -0,0 +1,39 @@
+package org.algorithms;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class findMaxConsecutiveOnes {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+
+    public int findMaxConsecutiveOnesExample(int[] nums) {
+        int max = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 1) {
+                count = 0;
+                continue;
+            }
+            count++;
+            max = Math.max(max, count);
+        //Or
+//            if(count > max){
+//                max = count;
+//            }
+        }
+        return max;
+    }
+
+
+    @Test
+    public void test() {
+        assertEquals(3, findMaxConsecutiveOnesExample(new int[]{1, 1, 0, 1, 1, 1}));
+        assertEquals(2, findMaxConsecutiveOnesExample(new int[]{1, 0, 1, 1, 0, 1}));
+        assertEquals(1, findMaxConsecutiveOnesExample(new int[]{1, 0, 1, 0, 1, 0}));
+    }
+}
